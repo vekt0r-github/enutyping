76bf00f0d6e520ca7b9803a13bab347c24eb19ae
Timmy Xiao <34635512+tzx@users.noreply.github.com>
2022-01-26 05:03:41 -0500

    Try on the homepage

--- frontend/src/components/pages/Home.tsx
+++ frontend/src/components/pages/Home.tsx
@@ -7,6 +7,7 @@ import { Navigate } from "react-router-dom";
 
 import gameplayVideo from "@/public/images/gameplay_sample.mp4"
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 type Props = {
   user: User | null,
@@ -26,6 +27,7 @@ const Padded = styled.div`
   max-width: 30%;
   margin-top: 12em;
   padding: 3rem;
+  z-index: 1;
 `;
 
 const Vid = styled.video`
@@ -40,6 +42,28 @@ const Title = styled.h1`
   color: white;
 `;
 
+const ButtonContainer = styled.div`
+  margin-top: 1.5em;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  justify-items: left;
+  align-items: left;
+`;
+
+const Button = styled(Link)`
+  font-size: 1.5em;
+  font-weight: bold;
+  text-decoration: none;
+  background-color: var(--teal);
+  border-radius: var(--s);
+  padding: 1.35rem;
+  color: var(--black0);
+  &:hover {
+    background-color: var(--lavender);
+    cursor: pointer;
+  }
+`;
+
 const Home = ({ user } : Props) => {
   if (user) {
     return <Navigate to="/play" replace={true} />
@@ -50,6 +74,9 @@ const Home = ({ user } : Props) => {
       <Padded>
         <Title>Type your favorite songs as you listen</Title>
         <p style={{color:"white"}}>the second bestest free-to-win rhythm game</p>
+        <ButtonContainer>
+          <Button to="/play">Try Now!</Button>
+        </ButtonContainer>
       </Padded>
       <Vid autoPlay loop muted playsInline src={gameplayVideo} />
     </Container>
